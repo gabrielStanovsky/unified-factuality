411f9a14b3ccdd4bd6292f65ad5b7736b933e18e
gabrielStanovsky <gabriel.satanovsky@gmail.com>
2017-05-17 17:49:03 +0300

    annotator runs without sudo

--- src/scripts/install_annotator.sh
+++ src/scripts/install_annotator.sh
@@ -1,19 +1,4 @@
 #!/bin/bash
 # Install required packages
-set -e
-
-# install props
-echo "Installing PropS..."
-wget https://github.com/gabrielStanovsky/props/archive/master.zip -O props.zip
-unzip -u props.zip
-pushd props-master
-python setup.py install
-popd
-
-# Install python packages
-echo "Installing requirements"
-pip install -r ./scripts/requirements.txt
-
-# Install spacy
-python -c "from spacy import __main__; __main__.cli_download('en')"
-#python -m spacy download en
+./_install_annotator.sh
+./_install_annotator.sh
